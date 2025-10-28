7e475f211dc20f9d42e4fd5bb5c6151e4e62b4ef
Fernanda Ferreira <fefcufe@gmail.com>
2025-10-28 12:04:45 -0300

    requirement 4 done

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -1,7 +1,13 @@
 const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  const lowStock = stockProducts.filter(product => product.quantityInStock <= 10 && product.quantityInStock > 0);
+  for (let i = 0; i < lowStock.length; i += 1) {
+    lowStock[i] = `${lowStock[i].productName}: ${lowStock[i].quantityInStock} unidades`;
+  }
+  return lowStock;
 };
+
+console.log(getLowStockProducts(stockProducts));
 
 module.exports = { getLowStockProducts };
