d5b4042d4211f04bb5927a75a8ac6f9c03b2e531
Fernanda Ferreira <fefcufe@gmail.com>
2025-10-28 09:43:02 -0300

    requirement 3 done: Create a function that returns an array with the names of products that are out of stock (quantityInStock equal to 0).

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -1,7 +1,13 @@
 const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  const outOfStock = stockProducts.filter(product => product.quantityInStock === 0);
+  for (let i = 0; i < outOfStock.length; i += 1) {
+    outOfStock[i] = outOfStock[i].productName;
+  }
+  return outOfStock; 
 };
+
+console.log(getOutOfStockProducts(stockProducts))
 
 module.exports = { getOutOfStockProducts };
