664ecf5dbdf2882f974230d5c8a25a1f61dba38e
Fernanda Ferreira <fefcufe@gmail.com>
2025-10-28 20:17:42 -0300

    requirement 6 done

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,19 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductByName = (name) => {
+
+  if (!name) {
+    return null;
+  } 
+  const result = stockProducts.find(product => product.productName === name);
+  if (!result) {
+    return null;
+  } else {
+    return {
+    description: result.description,
+    formattedPrice: `R$ ${result.price.toFixed(2)}`
+    }; 
+  }  
 };
 
 module.exports = { searchProductByName };
