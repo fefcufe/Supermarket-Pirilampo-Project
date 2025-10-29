4fba629abff09a4092cdf6459d3a3ab12e1a3f08
Fernanda Ferreira <fefcufe@gmail.com>
2025-10-29 12:36:23 -0300

    requirement 9 done

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,27 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
+  let resultArray = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (stockProducts[i].hasOwnProperty('allergyOrIntolerance')) {
+      const productWithAllergy = {
+        description: stockProducts[i].description,
+        formattedPrice: `R$ ${stockProducts[i].price}`,
+        allergyOrIntoleranceMessage: `Pode conter: ${stockProducts[i].allergyOrIntolerance.join(' ')}`
+      };
+      resultArray.push(productWithAllergy)
+    } else {
+      const productWithoutAllergy = {
+        description: stockProducts[i].description,
+        formattedPrice: `R$ ${stockProducts[i].price}`
+      };
+     
+      resultArray.push(productWithoutAllergy)
+    }
+  }
+  return resultArray;
 };
+
+getProductsWithAllergyOrIntoleranceInfo();
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
