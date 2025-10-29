23cd6a5f6bae675f9bd015d200630757df367632
Fernanda Ferreira <fefcufe@gmail.com>
2025-10-29 11:36:02 -0300

    requirement 8 done

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+  const productsOnSale = stockProducts.filter(product => product.onSale === true)
+  for (let i = 0; i < productsOnSale.length ; i += 1) {
+    
+    const newObject = {
+      description: productsOnSale[i].description,
+      formattedPrice: `R$ ${productsOnSale[i].price}`,
+      onSale: productsOnSale[i].onSale
+    }
+    productsOnSale[i] = newObject
+  }
+  return productsOnSale;
 };
+
 
 module.exports = { getProductsOnSale };
