c9397f0a6d74bf0a737abf0193147059b015c684
Fernanda Ferreira <fefcufe@gmail.com>
2025-10-29 11:28:02 -0300

    requirement 7 done

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,20 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
-  // Desenvolva seu código dentro dessa função...
+
+const searchProductsByBrand = (brandName) => {
+  const brandProducts = stockProducts.filter(product => product.brand === brandName);
+
+  for (let i = 0; i < brandProducts.length ; i += 1) {
+    
+    const newObject = {
+      description: brandProducts[i].description,
+      formattedPrice: `R$ ${brandProducts[i].price}`
+    }
+    brandProducts[i] = newObject
+  }
+  return brandProducts;
 };
+
+searchProductsByBrand('Hortifruti')
 
 module.exports = { searchProductsByBrand };
